3112cb6a060a9d347aa01a2cc6e0a8d8275d01df
@damnwidget <oscar.campos@member.fsf.org>
2013-04-14 21:58:32 +0100

    more changes to my_cnf

--- my_cnf.py
+++ my_cnf.py
@@ -156,9 +156,9 @@ max_binlog_size         = 100M
 #
 # InnoDB is enabled by default with a 10MB datafile in /var/lib/mysql/.
 # Read the manual for more InnoDB related options. There are many!
-default_storage_engine	= InnoDB
+# default_storage_engine	= InnoDB
 # you can't just change log file size, requires special procedure
-innodb_log_file_size    = 50M
+# innodb_log_file_size    = 50M
 innodb_buffer_pool_size = 256M
 innodb_flush_log_at_trx_commit=2
 innodb_log_buffer_size	= 8M
